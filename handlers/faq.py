f94f1fde9ee14bb5eaa2526016479366f3cdf507
bigbob52 <egor.mershiev@gmail.com>
2025-09-02 17:57:45 +0300

    Added FAQ handler

---
+++ handlers/faq.py
@@ -0,0 +1,13 @@
+from aiogram import types, Router, F
+from aiogram.types import CallbackQuery
+from .keyboards.faq import faq_kb
+
+router = Router()
+
+
+@router.callback_query(F.data == "faq")
+async def show_faq(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text="Выберите интересующий Вас вопрос из списка ниже",
+        reply_markup=faq_kb
+    )
