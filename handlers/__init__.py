342ba9a32f132de87c7055a7cc9350373b72e94c
bigbob52 <egor.mershiev@gmail.com>
2025-09-02 17:58:28 +0300

    Updated initialization handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,9 @@
 from aiogram import Dispatcher
-from . import start, new_order, rate, account
+from . import start, new_order, rate, account, faq
 
 def register_user_handlers(dp: Dispatcher):
     dp.include_router(start.router)
     dp.include_router(new_order.router)
     dp.include_router(rate.router)
     dp.include_router(account.router)
+    dp.include_router(faq.router)
