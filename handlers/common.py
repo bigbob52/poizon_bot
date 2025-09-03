e90015995cc914b0bff5e72688efdebf48ced9a3
bigbob52 <egor.mershiev@gmail.com>
2025-09-03 23:00:39 +0300

    Added main menu output

---
+++ handlers/common.py
@@ -0,0 +1,20 @@
+from aiogram.types import CallbackQuery, Message
+from keyboards.menu import get_menu_for_user
+
+
+async def send_main_menu(user_id: int, destination: CallbackQuery | Message):
+    """Отправка главного меню пользователю"""
+    kb = get_menu_for_user(user_id)
+    if isinstance(destination, Message):
+        await destination.answer(
+            text="Приветствую! 👋\n"
+                 "Я ваш помощник в оформлении заказов. Выберите нужное действие из меню ниже.",
+            reply_markup=kb
+        )
+    elif isinstance(destination, CallbackQuery):
+        await destination.message.edit_text(
+            text="Приветствую! 👋\n"
+                 "Я ваш помощник в оформлении заказов. Выберите нужное действие из меню ниже.",
+            reply_markup=kb
+        )
+        await destination.answer()
